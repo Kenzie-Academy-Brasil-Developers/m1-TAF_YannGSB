6242ce878d58a34c385c059daad4e8cede0b8d9b
unknown <yanngsb@hotmail.com>
2023-06-14 21:42:00 -0300

    Tarefa 2 - Estruturas condicionais e operadores

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ let passed = false;
     ...
 */
 // **sua lógica a partir daqui**
+if (gender === "male" && height >= 1.70 && (barReps >= 6 || barSeconds >= 15) && abs >= 41 && ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <=1200)) && ((swimDistance >= 100 && swimTime <=60) || (diveTime <= 30))) {
+    passed = true
+}
+
+if (gender === "female" && height >= 1.60 && (barReps >= 5 || barSeconds >= 12) && abs >= 41 && ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <=1320)) && ((swimDistance >= 100 && swimTime <=60) || (diveTime <= 30))) {
+    passed = true
+}
 
 /*
     Seu código deve conter apenas UM console.log, e ele deve ser o abaixo.
